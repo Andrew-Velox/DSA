19a820a882ad73702dab8b3aa616262263445f5e
Velox <mohabbat.bd2020@gmail.com>
2024-05-30 01:11:38 +0600

    Create BFS_Using_Queue_Array.cpp

---
+++ Graph Theory/Breadth First Search (BFS)/BFS_Using_Queue_Array.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mx = 100;
+int vi[mx][mx];
+bool visit[mx];
+
+const int QUEUE_SIZE = mx;
+int queueArr[QUEUE_SIZE];
+int front = 0;
+int rear = -1;
+int itemCount = 0;
+
+bool isQueueEmpty() {
+    return itemCount == 0;
+}
+
+bool isQueueFull() {
+    return itemCount == QUEUE_SIZE;
+}
+
+void enqueue(int data) {
+    if (!isQueueFull()) {
+        if (rear == QUEUE_SIZE - 1) {
+            rear = -1;
+        }
+        queueArr[++rear] = data;
+        itemCount++;
+    }
+}
+
+int dequeue() {
+    int data = queueArr[front++];
+    if (front == QUEUE_SIZE) {
+        front = 0;
+    }
+    itemCount--;
+    return data;
+}
+
+void bfs(int st, int n) {
+    enqueue(st);
+    visit[st] = true;
+
+    while (!isQueueEmpty()) {
+        int surur_val = dequeue();
+
+        cout << surur_val << " ";
+
+        for (int x = 1; x <= n; x++) {
+            if (vi[surur_val][x] && !visit[x]) {
+                enqueue(x);
+                visit[x] = true;
+            }
+        }
+    }
+}
+
+int main() {
+    int node, edge;
+    cin >> node >> edge;
+
+
+    while (edge--) {
+        int a, b;
+        cin >> a >> b;
+        vi[a][b] = 1;
+        vi[b][a] = 1;
+    }
+
+    for(int x=0; x<=node; x++) visit[x] = false;
+
+    cout << "Connected Nodes" << endl;
+    for(int x = 1; x <= node; x++){
+        cout << "\t\t";
+        for(int y = 1; y <= node; y++){
+            cout << vi[x][y] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    int start;
+    cin >> start;
+
+    cout << "BFS Traversal: ";
+    bfs(start, node);
+    cout << endl;
+
+    return 0;
+}
+
