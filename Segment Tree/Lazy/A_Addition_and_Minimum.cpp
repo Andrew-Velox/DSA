c69f83f4acb929fdbb6aef758dc10a43556a9fe2
Mohabbat <mohabbat.bd2020@gmail.com>
2025-11-03 20:58:49 +0600

    Add files via upload

---
+++ Segment Tree/Lazy/A_Addition_and_Minimum.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+#ifdef velox
+#include "debug.h"
+#else
+#define dbg(x...)
+#endif
+
+
+const ll N = 1e5+10;
+vector<ll> v(N),t(4*N),lazy(4*N);
+
+void marge(ll n, ll b, ll e){
+    if(lazy[n]==0) return;
+
+    t[n]+=lazy[n];
+    if(b!=e){
+        ll l=n*2;
+        ll r=(n*2)+1;
+        lazy[l]+=lazy[n];
+        lazy[r]+=lazy[n];
+    }
+    lazy[n]=0;
+}
+
+void make(ll n,ll b, ll e){
+    if(b==e){
+        t[n]=v[b];
+        return;
+    }
+
+    ll mid=(b+e)/2;
+    ll l=(n*2);
+    ll r=(n*2)+1;
+
+    make(l,b,mid);
+    make(r,mid+1,e);
+    t[n]= min(t[l],t[r]);
+}
+
+
+void update(ll n,ll b,ll e, ll i,ll j, ll val){
+    marge(n,b,e); // lazy
+    if(i>e || j<b) return;
+
+    if(i<=b && j>=e){
+        lazy[n] = val;
+        marge(n,b,e); // lazy
+        return;
+    }
+
+    ll mid=(b+e)/2;
+    ll l=(n*2);
+    ll r=(n*2)+1;
+
+    update(l,b,mid,i,j,val);
+    update(r,mid+1,e,i,j,val);
+    t[n]= min(t[l],t[r]);
+
+}
+
+
+ll query(ll n, ll b, ll e, ll i, ll j){
+    marge(n,b,e); // lazy
+    if(i>e || j<b) return INT_MAX;
+
+    if(i<=b && j>=e) return t[n];
+
+    ll mid=(b+e)/2;
+    ll l=(n*2);
+    ll r=(n*2)+1;
+
+    return min(query(l,b,mid,i,j),query(r,mid+1,e,i,j));
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    
+    ll len,q; cin >> len >> q;
+    make(1,1,len);
+
+    while(q--){
+        ll op; cin >> op;
+        if(op==1){
+            ll l,r,val; cin >> l >> r >> val;
+            l++;
+            update(1,1,len,l,r,val);
+        }
+        else{
+            ll i,j; cin >> i >> j;
+            i++;
+            cout << query(1,1,len,i,j) << endl;
+        }
+    }
+    
+    
+    return 0;
+}
---
+++ Segment Tree/Lazy/B_Multiplication_and_Sum.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+#ifdef velox
+#include "debug.h"
+#else
+#define dbg(x...)
+#endif
+
+const ll MOD=1e9+7;
+
+const ll N = 1e5+10;
+vector<ll> v(N),t(4*N),lazy(4*N);
+
+void marge(ll n, ll b, ll e){
+    if(lazy[n]==1) return;
+
+    t[n]=(t[n]*lazy[n])%MOD;
+    if(b!=e){
+        ll l=n*2;
+        ll r=(n*2)+1;
+        lazy[l]=(lazy[l]*lazy[n])%MOD;
+        lazy[r]=(lazy[r]*lazy[n])%MOD;
+    }
+    lazy[n]=1;
+}
+
+ll op2(ll l,ll r){
+    return (l+r)%MOD;
+}
+void make(ll n,ll b, ll e){
+    lazy[n]=1;
+    if(b==e){
+        t[n]=1;
+        return;
+    }
+
+    ll mid=(b+e)/2;
+    ll l=(n*2);
+    ll r=(n*2)+1;
+
+    make(l,b,mid);
+    make(r,mid+1,e);
+    t[n]= op2(t[l],t[r]);
+}
+
+
+void update(ll n,ll b,ll e, ll i,ll j, ll val){
+    marge(n,b,e); // lazy
+    if(i>e || j<b) return;
+
+    if(i<=b && j>=e){
+        lazy[n] = val;
+        marge(n,b,e); // lazy
+        return;
+    }
+
+    ll mid=(b+e)/2;
+    ll l=(n*2);
+    ll r=(n*2)+1;
+
+    update(l,b,mid,i,j,val);
+    update(r,mid+1,e,i,j,val);
+    t[n]= op2(t[l],t[r]);
+
+}
+
+
+ll query(ll n, ll b, ll e, ll i, ll j){
+    marge(n,b,e); // lazy
+    if(i>e || j<b) return 0;
+
+    if(i<=b && j>=e) return t[n];
+
+    ll mid=(b+e)/2;
+    ll l=(n*2);
+    ll r=(n*2)+1;
+
+    return op2(query(l,b,mid,i,j),query(r,mid+1,e,i,j));
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    
+    ll len,q; cin >> len >> q;
+    make(1,1,len);
+
+    while(q--){
+        ll op; cin >> op;
+        if(op==1){
+            ll l,r,val; cin >> l >> r >> val;
+            l++;
+            update(1,1,len,l,r,val);
+        }
+        else{
+            ll i,j; cin >> i >> j;
+            i++;
+            cout << query(1,1,len,i,j) << endl;
+        }
+    }
+    
+    
+    return 0;
+}
